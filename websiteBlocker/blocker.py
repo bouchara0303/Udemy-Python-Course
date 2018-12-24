1e9e07bb59631b25a3938dba2cc34c3d17dd31f0
bouchara0303 <austinabouchard@gmail.com>
2018-12-23 18:44:30 -0600

    Made program easier to read

--- websiteBlocker/blocker.py
+++ websiteBlocker/blocker.py
@@ -1,22 +1,26 @@
-"""
-This program does it's job of updating the hosts file on mac, however,
-this is not a effective method of blocking websites in general...
-"""
+#This program does it's job of updating the hosts file on OSX, however,
+#this is not a effective method of blocking websites in general...
 
 import time
 from datetime import datetime as dt
 
+#Hosts file path will likely be different on different OS
 hostsPath = '/private/etc/hosts'
 redirect = '127.0.0.1'
-redirect1 = '::1'
+redirectIPV6 = '::1'
 
-websiteBlockList = ['youtube.com', 'www.youtube.com', 'http://www.youtube.com', 'https://www.youtube.com', 'http://youtube.com', 'https://youtube.com']
+#Change to desired websites to be blocked. Include both www version and non www version of url
+#(eg. ['www.example.com', 'example.com']).
+websiteBlockList = ['youtube.com', 'www.youtube.com']
 
-while True: 
-        # Establish multiple datetime objects
+while True:
+        #Establish datetime objects
         now = dt.now()
+
+        #Change hours of date time object to specify hours of website blocking
         start = dt(now.year, now.month, now.day, 10)
         finish = dt(now.year, now.month, now.day, 12)
+        
         if (start < now < finish):
             print('You should be working...')
             with open(hostsPath, 'r+') as hosts:
@@ -25,10 +29,10 @@ while True:
                     if website in buffer:
                         pass
                     else:
-                        hosts.write(redirect + '    ' + website + '\n')
-                        hosts.write(redirect1 + '    ' + website + '\n')    
+                        hosts.write(redirectIPV4 + '    ' + website + '\n')
+                        hosts.write(redirectIPV6 + '    ' + website + '\n')
         else:
-            print('Time for some fun...')
+            print('Time for some fun!')
             with open(hostsPath, 'r+') as hosts:
                 buffer = hosts.readlines()
                 hosts.seek(0)
@@ -36,4 +40,5 @@ while True:
                     if not any(websites in lines for websites in websiteBlockList):
                         hosts.write(lines)
                 hosts.truncate()
+        #Checks every 5 seconds
         time.sleep(5)
